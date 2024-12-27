21032fc63763387ea18afceac9e240ffa8a9fde5
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-27 15:08:24 +0000

    day three function case statement written

--- advent_of_code_2014/day_three/day_three.py
+++ advent_of_code_2014/day_three/day_three.py
@@ -2,4 +2,20 @@
 
 def santa_tracker(string: str) -> int:
     """Returns how many houses santa visited at least once."""
-    ...
+
+    coords_visited = [(0,0)]
+    coord = (0,0)
+
+    for character in string:
+        match character:
+            case "^":
+                coord[1] += 1
+            case ">":
+                coord[0] += 1
+            case "<":
+                coord[0] -= 1
+            case "v":
+                coord[1] -= 1
+
+        coords_visited.append(coord)
+        
