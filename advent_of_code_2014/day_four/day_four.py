33224382bf548dab2ec60fc3e713202d7a2f9dbc
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-27 18:21:08 +0000

    day_four pt2 first attempt

--- advent_of_code_2014/day_four/day_four.py
+++ advent_of_code_2014/day_four/day_four.py
@@ -2,8 +2,9 @@
 
 import hashlib
 
-def hash_converter(key: str) -> str:
-    """Returns the thing that this problem is asking for"""
+def hash_converter(key: str, part_two=False) -> str:
+    """Returns numbers after the key that correspond to
+    the MD5 hash that has 5 (False) or 6 (True) leading 0's"""
 
     found = False
     i = 0
@@ -13,9 +14,13 @@ def hash_converter(key: str) -> str:
 
         hash_string = hashlib.md5(string.encode()).hexdigest()
 
-        if hash_string[0:5] == "00000":
-            found = True
-            break
+        if not part_two:
+            if hash_string[0:5] == "00000":
+                found = True
+        
+        else:
+            if hash_string[0:6] == "000000":
+                found = True
 
         string = key + str(i)
         i += 1
@@ -24,4 +29,4 @@ def hash_converter(key: str) -> str:
 
 if __name__ == '__main__':
 
-    print(hash_converter('ckczppom'))
+    print(hash_converter('ckczppom', True))
