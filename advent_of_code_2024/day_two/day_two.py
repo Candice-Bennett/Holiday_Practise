9e2658c9264a051835c388de6aebcdb0ecdcf6b9
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-27 21:14:11 +0000

    linted day two 2024

--- advent_of_code_2024/day_two/day_two.py
+++ advent_of_code_2024/day_two/day_two.py
@@ -6,20 +6,20 @@ def part_one(list_of_num):
     ascending = False
 
     if list_of_num[0] < list_of_num[1]:
-            ascending = True
+        ascending = True
 
     for i in range(len(list_of_num)-1):
 
         if ascending:
 
-            if not (list_of_num[i] < list_of_num[i+1] <= list_of_num[i] + 3):
-                 return False
+            if not list_of_num[i] < list_of_num[i+1] <= list_of_num[i] + 3:
+                return False
 
         else:
 
-            if not (list_of_num[i] > list_of_num[i+1] >= list_of_num[i] - 3):
-                 return False
-    
+            if not list_of_num[i] > list_of_num[i+1] >= list_of_num[i] - 3:
+                return False
+
     return True
 
 if __name__ == '__main__':
@@ -34,10 +34,10 @@ if __name__ == '__main__':
     for puzzle in input_data:
         num_list = puzzle.split(' ')
 
-        for i in range(len(num_list)):
-            num_list[i] = int(num_list[i])
+        for j in range(len(num_list)):
+            num_list[j] = int(num_list[j])
 
         if part_one(num_list):
             total += 1
-    
+
     print(total)
