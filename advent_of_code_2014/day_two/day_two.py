fbc1abaa6daaa34afea658dc207b3e07ef01d9d1
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-27 00:56:02 +0000

    day two tests almost finished

--- advent_of_code_2014/day_two/day_two.py
+++ advent_of_code_2014/day_two/day_two.py
@@ -1,7 +1,19 @@
 """File containing solutions to advent of code 2014 day two."""
 
-def elevators(string: str) -> int:
-    """"""
+def surface_area(dimensions: list[int]) -> int:
+    """Takes the three dimensions of an object and 
+    returns the area of wrapping paper needed."""
+    ...
+
+
+def validate_surface_area(dimensions: list[int]) -> bool:
+    """Returns whether input is of valid format."""
+    ...
+
+
+def convert_data(string: str) -> list[int]:
+    """Returns an array with values from input string of form AxBxC
+    Where A, B and C are numbers"""
     ...
 
 if __name__ == "__main__":
--- advent_of_code_2014/day_two/test_day_two.py
+++ advent_of_code_2014/day_two/test_day_two.py
@@ -1,3 +1,24 @@
 #pylint: skip-file
 
-from advent_of_code_2014.day_two.day_two import
+import pytest
+from advent_of_code_2014.day_two.day_two import surface_area, validate_surface_area, convert_data
+
+def test_surface_area_correct():
+    assert surface_area('2x3x4') == 58
+    assert surface_area('1x1x10') == 43
+
+def test_validate_surface_area_bad_input():
+
+    assert validate_surface_area([1,2,3,4,5]) == False
+    assert validate_surface_area(['one',2,3]) == False
+    assert validate_surface_area(True,2,3) == False
+    assert validate_surface_area('1',2,3) == False
+
+def test_validate_surface_area_good_input():
+
+    assert validate_surface_area([1,2,3]) == True
+    assert validate_surface_area([1.1,2.1,3.1]) == True
+
+def test_convert_data_positive():
+
+    assert convert_data
