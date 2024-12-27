73ff1e841d9b59c1f77d4351d2de37f2cd4c5208
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-27 16:15:52 +0000

    rewrote tests to account for changing of solution for pt2 of day three

--- advent_of_code_2014/day_three/day_three.py
+++ advent_of_code_2014/day_three/day_three.py
@@ -1,7 +1,7 @@
 """Contains functions to solve advent of code 2014 day three."""
 
-def santa_tracker(string: str) -> int:
-    """Returns how many houses santa visited at least once."""
+def santa_tracker(string: str) -> list[list[int]]:
+    """Returns the coordinates santa visits."""
 
     coords_visited = [[0,0]]
     coord = [0,0]
@@ -20,18 +20,35 @@ def santa_tracker(string: str) -> int:
 
         coords_visited.append(coord[:])
 
+
+def coord_splitter(directions: str) -> list[list[str]]:
+    """Takes the directions and splits them into into
+    instructions for santa and robo-santa."""
+    ...
+
+
+def unique_coord_count(coords: list[list[int]]) -> int:
+    """Takes an input of coordinates and returns the number of unique ones"""
+    
     #ideally I'd use a map or something to clean the coords_visited array but
     #I don't know the syntax and am writing this on the road
     unique_coords = []
-    for co in coords_visited:
-        if co not in unique_coords:
-            unique_coords.append(co)
+    for coord in coords:
+        if coord not in unique_coords:
+            unique_coords.append(coord)
 
     return len(unique_coords)
+
+
+def robo_santa_tracker(directions: str) -> int:
+    """Takes coordinates and returns the number of houses visited when
+    santa and robo-santa are both delivering presents."""
+    ...
+
 
 if __name__ == "__main__":
 
     with open('day_three_input.txt', 'r', encoding='UTF-8') as file:
         input_data = file.read()
 
-    print(santa_tracker(input_data))
+    print(unique_coord_count(santa_tracker(input_data)))
--- advent_of_code_2014/day_three/test_day_three.py
+++ advent_of_code_2014/day_three/test_day_three.py
@@ -1,8 +1,17 @@
 #pylint: skip-file
 
-from day_three import santa_tracker
+from day_three import santa_tracker, coord_splitter, robo_santa_tracker
 
 def test_santa_tracker():
-    assert santa_tracker(">") == 2
-    assert santa_tracker("^>v<") == 4
-    assert santa_tracker("^v^v^v^v^v") == 2
+    assert santa_tracker(">") == [[0,0], [1,0]]
+    assert santa_tracker("^>v<") == [[0,0], [0,1], [1,1], [1,0], [0,0]]
+
+def test_coord_splitter():
+    assert coord_splitter("^^^") == [["^^"],["^"]]
+    assert coord_splitter("><^v^v<>") == [[">^^<"],["<vv>"]]
+    assert coord_splitter("^v^v^v^v") == [["^^^^"],["vvvv"]]
+
+def test_robo_santa_tracker():
+    assert robo_santa_tracker('^v') == 3
+    assert robo_santa_tracker('^>v<') == 3
+    assert robo_santa_tracker('^v^v^v^v^v') == 11
