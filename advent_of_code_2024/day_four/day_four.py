7f6ac1755ae8e8df7406a84d0afdf2b68604dadb
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-30 01:32:29 +0000

    day four function one written

--- advent_of_code_2024/day_four/day_four.py
+++ advent_of_code_2024/day_four/day_four.py
@@ -4,9 +4,17 @@
 #       that way would only need find horizontal, vertical, diagonal and just
 #       input correct and reversed?
 
+import re
+
 def find_horizontal(grid: list[str], reverse=False) -> int:
     """Returns the number of horizontal XMAS instances"""
-    ...
+    
+    total_xmas = 0
+    regex = re.compile(r'XMAS')
+    for line in grid:
+        total_xmas += len(regex.findall(line))
+    
+    return total_xmas
 
 
 def find_vertical(grid: list[str], reverse=False) -> int:
@@ -17,4 +25,13 @@ def find_vertical(grid: list[str], reverse=False) -> int:
 def find_diagonal(grid: list[str], reverse=False) -> int:
     """Returns the number of diagonal XMAS instances"""
     ...
+
+if __name__ == "__main__":
+
+    with open('day_four_input.txt', 'r', encoding='UTF-8') as file:
+        input_data = file.read()
+    
+    split_data = input_data.split('\n')
+    for line in split_data:
+        print(line)
 
