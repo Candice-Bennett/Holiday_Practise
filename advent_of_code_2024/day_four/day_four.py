eea82cbb7f3abf50d3b2992537589a20cbb8d739
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-30 01:42:54 +0000

    vertical function written.

--- advent_of_code_2024/day_four/day_four.py
+++ advent_of_code_2024/day_four/day_four.py
@@ -25,7 +25,16 @@ def find_horizontal(grid: list[str], reverse=False) -> int:
 
 def find_vertical(grid: list[str], reverse=False) -> int:
     """Returns the number of vertical XMAS instances"""
-    ...
+    
+    rotated_grid = [''] * len(grid)
+
+    #god I wish I could do this in MATLAB LOL
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            rotated_grid[j] += grid[i][j]
+    
+    return find_horizontal(rotated_grid, reverse)
+
 
 
 def find_diagonal(grid: list[str], reverse=False) -> int:
@@ -36,8 +45,19 @@ if __name__ == "__main__":
 
     with open('day_four_input.txt', 'r', encoding='UTF-8') as file:
         input_data = file.read()
-    
+
     split_data = input_data.split('\n')
     for line in split_data:
         print(line)
 
+    print(len(split_data))
+    print(len(line))
+
+    new_grid = ['XXXXX',
+                'MMMMM',
+                'AAAAA',
+                'SSSSS',
+                'XXXXX']
+
+    print(new_grid)
+    find_vertical(new_grid)
