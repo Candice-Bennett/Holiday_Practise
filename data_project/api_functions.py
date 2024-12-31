72faeb2bf6ec3161c1877cdaa6f86d004799c7cb
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-31 20:14:59 +0000

    getting data from the api now!

--- data_project/api_functions.py
+++ data_project/api_functions.py
@@ -1,8 +1,10 @@
 """Script contains functions needed to interact with IGBD API."""
 
-from requests import post
 from os import environ
+from requests import post
 from dotenv import load_dotenv
+
+BASE_URL = 'https://api.igdb.com/v4'
 
 def generate_token() -> dict:
     """Generates a Twitch auth token."""
@@ -16,6 +18,29 @@ def generate_token() -> dict:
         f"grant_type=client_credentials"
     )
 
-    response = post(get_token_string)
+    response = post(get_token_string, timeout=60)
 
     return response.json()
+
+
+def get_info_test() -> dict:
+    """Tests API request."""
+
+    load_dotenv('.env')
+
+    query_string = BASE_URL + '/games'
+
+    headers = {
+        'Client-ID': environ["TWITCH_CLIENT_ID"],
+        'Authorization': "Bearer " + environ["TWITCH_ACCESS_TOKEN"]
+    }
+
+    body = {"fields": "*"}
+
+    # Use `json` instead of `data`
+    response = post(query_string, headers=headers, json=body, timeout=60)
+
+    return response.json()
+
+if __name__ == '__main__':
+    print(get_info_test())
