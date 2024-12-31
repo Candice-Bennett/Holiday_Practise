9004bedf4f37b4f25b3743d6719239f0d5abc5b0
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-31 17:15:46 +0000

    extraction functions written, refactoring now

--- advent_of_code_2024/day_five/day_five.py
+++ advent_of_code_2024/day_five/day_five.py
@@ -2,7 +2,35 @@
 def extract_rules(data: str) -> list[list[int]]:
     """Extracts rules of the input."""
 
+    split_data = data.split('\n')
+
+    rules = []
+    for line in split_data:
+        if '|' in line:
+            rule = line.split('|')
+            rules.append([int(rule[0]),int(rule[1])])
+    
+    return rules
+
 
 def extract_sequences(data: str) -> list[list[int]]:
     """Extracts sequences of the input"""
 
+    split_data = data.split('\n')
+
+    sequences = []
+    for line in split_data:
+        if ',' in line:
+            sequence = line.split(',')
+            sequences.append(int(sequence[:]))
+    
+    return sequences
+
+if __name__ == '__main__':
+
+    with open('day_five_input.txt','r',encoding='UTF-8') as file:
+        input_data = file.read()
+    
+    print(input_data)
+    print(extract_rules(input_data))
+    print(extract_sequences(input_data))
--- advent_of_code_2024/day_five/test_day_five.py
+++ advent_of_code_2024/day_five/test_day_five.py
@@ -4,7 +4,7 @@ from day_five import extract_rules, extract_sequences
 
 def test_rule_extractor():
     assert extract_rules('93|53') == [[93,53]]
-    assert extract_rules('''47|53
+    assert sorted(extract_rules('''47|53
 97|13
 97|61
 97|47
@@ -31,7 +31,7 @@ def test_rule_extractor():
 75,29,13
 75,97,47,61,53
 61,13,29
-97,13,75,29,47''') == [[97,13],
+97,13,75,29,47''')) == sorted([[97,13],
 [97,61],
 [97,47],
 [75,29],
@@ -50,7 +50,7 @@ def test_rule_extractor():
 [75,61],
 [47,29],
 [75,13],
-[53,13]]
+[53,13]])
 
 def test_sequences_extractor():
     assert extract_sequences('75,47,61,53,29') == [[75,47,61,53,29]]
