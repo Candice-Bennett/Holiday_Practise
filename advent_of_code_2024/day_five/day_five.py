88e8d426353b5051629bf0bca23b09807b67402a
Candice-Bennett <Candice.Bennett123@gmail.com>
2024-12-31 17:07:44 +0000

    day 5 2024 extraction  function tests written

---
+++ advent_of_code_2024/day_five/day_five.py
@@ -0,0 +1,8 @@
+"""Script that contains solutions to day 5 2024."""
+def extract_rules(data: str) -> list[list[int]]:
+    """Extracts rules of the input."""
+
+
+def extract_sequences(data: str) -> list[list[int]]:
+    """Extracts sequences of the input"""
+
