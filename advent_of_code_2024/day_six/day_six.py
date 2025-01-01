7389cf405159561337651735a20b71e4b50db931
Candice-Bennett <Candice.Bennett123@gmail.com>
2025-01-01 19:59:36 +0000

    day six 2024 begun, tests written for three functions about to begin writing functions.

---
+++ advent_of_code_2024/day_six/day_six.py
@@ -0,0 +1,16 @@
+"""Script containing all the functions for day six."""
+
+def locate_officer(grid: list[str]) -> list[int]:
+    """Takes the grid and returns the coordinate [x,y] where the officer is
+    where +x is eastwardly and +y is southern."""
+    ...
+
+
+def locate_obstructions(grid: list[str]) -> list[list[int]]:
+    """Returns all the grid coordinates of the obstructions."""
+    ...
+
+
+def part_one_solution(grid_string: str) -> int:
+    """Returns the number of spaces visited by the officer."""
+    ...
