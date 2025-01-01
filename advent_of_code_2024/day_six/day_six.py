f64fe0ad75d8fa69a09cf35b832cd31d9b551e8d
Candice-Bennett <Candice.Bennett123@gmail.com>
2025-01-01 20:18:22 +0000

    reworked find function

--- advent_of_code_2024/day_six/day_six.py
+++ advent_of_code_2024/day_six/day_six.py
@@ -5,22 +5,20 @@ def comprehend_input(grid_string: str) -> list[str]:
     return grid_string.split('\n')
 
 
-def locate_officer(grid: list[str]) -> list[int]:
+def locate_item(grid: list[str], item: str) -> list[int]:
     """Takes the grid and returns the coordinate [x,y] where the officer is
     where +x is eastwardly and +y is southern."""
     
+    coords = []
+
     for i in range(len(grid)):
-        if '^' in grid[i]:
+        if item in grid[i]:
             for j in range(len(grid[i])):
-                if grid[i][j] == '^':
-                    return [i,j]
+                if grid[i][j] == item:
+                    coords.append([i,j])
     
-    raise ValueError('This input does not contain an officer')
+    return coords
 
-
-def locate_obstructions(grid: list[str]) -> list[list[int]]:
-    """Returns all the grid coordinates of the obstructions."""
-    
 
 def part_one_solution(grid_string: str) -> int:
     """Returns the number of spaces visited by the officer."""
